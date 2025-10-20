27066fdea9de7fe832c3104b5b8c18858a174dbc
monojitgoswami69 <monojit.goswami.0905@gmail.com>
2025-10-20 13:16:24 +0530

    tweaks

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -13,9 +13,6 @@ if status is-interactive # Commands to run in interactive sessions can go here
 
     # Use starship
     starship init fish | source
-    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-    end
 
     # Aliases
     alias pamcan pacman
